5ff976ba60c5cf71fe6ac804606f62f137db8fb8
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-12-07 14:06:24 +0000

    lazy loading components via export index

---
+++ src/lazy.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Component2 } from './some-components';
+
+export default () => {
+  return (
+    <div>
+      Lazy content
+      <Component2 />
+    </div>
+  );
+}
---
+++ src/some-components/component-one/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      Component 1
+    </div>
+  );
+}
